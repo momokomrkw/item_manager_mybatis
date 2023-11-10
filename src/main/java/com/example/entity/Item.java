32ce690d8e8b65317592f3b427aa139583984f2b
Morikawa Momoko <mmk.muraoka@gmail.com>
2023-11-10 14:19:00 +0900

    firstCommit

---
+++ src/main/java/com/example/entity/Item.java
@@ -0,0 +1,59 @@
+package com.example.entity;
+
+import java.time.LocalDateTime;
+
+public class Item {
+	private int id;
+	private String name;
+	private int price;
+	private LocalDateTime deletedAt;
+	private int categoryId;
+	private int stock;
+	
+	private Category category;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name =name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public int getStcok() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+}
